94d029218d0f05a4427588e8cb6ac7ae3ea4e55f
ramonvaleriano <rrvaleriano@gmail.com>
2021-02-24 14:11:28 -0300

    Editando Jogos
    Criando um rota para Editar jogo e Atualizar dados no servidor

--- jogoteca.py
+++ jogoteca.py
@@ -40,6 +40,22 @@ def criar():
     jogo_dao.salvar(jogo)
     return redirect(url_for('index'))
 
+@app.route('/atualizar', methods=['POST',])
+def atualizar():
+    nome = request.form['nome']
+    categoria = request.form['categoria']
+    console = request.form['console']
+    jogo = Jogo(nome, categoria, console, id=request.form['id'])
+    jogo_dao.salvar(jogo)
+    return redirect(url_for('index'))
+
+@app.route('/editar/<int:id>')
+def editar(id):
+    if 'usuario_logado' not in session or session['usuario_logado'] == None:
+        return redirect(url_for('login', proxima=url_for('editar')))
+    jogo = jogo_dao.busca_por_id(id)
+    return render_template('editar.html', titulo='Editando Jogo', jogo=jogo)
+
 
 @app.route('/login')
 def login():
